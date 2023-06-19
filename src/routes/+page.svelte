56a5e24866a03f46479c6b511470ee578a2beb55
Domenik Reitzner <domenik@reitzner.at>
2023-06-19 16:23:38 +0200

    Finish up list ♎

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { enhance } from '$app/forms';
+	import ListItem from './ListItem.svelte';
 
     export let data;
 </script>
@@ -7,24 +8,36 @@
 <h2>Add Todo</h2>
 <form action="?/addTodo" method="post" use:enhance>
     <input type="text" name="newTodo">
-    <button type="submit">Add</button>
+    <button type="submit">Add Todo</button>
 </form>
-<h2>Open Todos</h2>
-<ul>
-    {#each Object.entries(data.items).filter(([name, checked]) => !checked) as [name]}
-    <li>
-        form
-        {name}
-    </li>
-    {:else}
-    <h3>No todos</h3>
-    {/each}
-</ul>
-<h2>Done Todos</h2>
-<ul>
-    {#each Object.entries(data.items).filter(([name, checked]) => checked) as [name]}
-    <li>{name}</li>
-    {:else}
-    <h3>No todos</h3>
-    {/each}
-</ul>
+<section class="row">
+    <div class="columns six">
+        <h2>Open Todos</h2>
+        <ul>
+            {#each Object.entries(data.items).filter(([name, checked]) => !checked) as [name]}
+            <ListItem {name} />
+            {:else}
+            <h3>No todos</h3>
+            {/each}
+        </ul>
+    </div>
+    <div class="columns six">
+        <h2>Done Todos</h2>
+        <ul>
+            {#each Object.entries(data.items).filter(([name, checked]) => checked) as [name]}
+            <ListItem {name} checked />
+            {:else}
+            <h3>No todos</h3>
+            {/each}
+        </ul>
+    </div>
+</section>
+
+<style>
+    ul {
+        list-style: none;
+    }
+    button {
+        font-size: 2rem;
+    }
+</style>
---
+++ src/routes/ListItem.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { enhance } from '$app/forms';
+
+    /**
+     * @type {string}
+     */
+    export let name;
+    /**
+     * @type {boolean}
+     */
+    export let checked = false;
+</script>
+<li>
+    <form method="post" use:enhance>
+        <p>{name}</p>
+        <div>
+            <input type="hidden" name="todo" value={name}>
+            <button formaction="?/checkTodo">
+            {#if checked}
+            uncheck
+            {:else}
+            #done
+            {/if}
+            </button>
+            <button formaction="?/deleteTodo">🗑</button>
+        </div>
+    </form>
+</li>
+
+<style>
+    button {
+        font-size: 2rem;
+    }
+    form {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
